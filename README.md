a5e7d0dd2127b5c93550b6cd754166ac47433c84
Kaleb Clark <kaleb@abraxxus.net>
2013-11-17 10:14:08 -0800

    Implemented Text::CSV module

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 This code prepares Sacramento Crime extracts from: (http://www.sacpd.org/crime/stats/) to be inserted into a MySQL database.
 
+The crime extracts are available via CSV file. The unique identifier in these CSV's would be a VARCHAR in MySQL, this code inserts a new INT column that can be configured as a PRIMARY key. The date format is also not optimal for MySQL. This code converts to a "yyyy-mm-dd hh:mm:ss" format for MySQL
+
 The output will be in MySQL INSERT format.
 
 ## Requirements
 Perl module: Text::CSV
-This is required because many of the fields contain quotes.
+This is required because some of the fields contain quotes & commas.
 
 ## Usage
 ./crime-parse.pl <MySQL tablename> <infile> <outfile>
--- crime-parse.pl
+++ crime-parse.pl
@@ -2,37 +2,46 @@
 
 use Data::Dumper;
 use Text::CSV;
-exit;
 
 print "Usage: $0 <tablename> <infile> <outfile>\n";
 open(FILE, $ARGV[1]) || die "Can't open input file - $!\n";
+my $csv = Text::CSV->new();
 open(OUTFILE, '>'.$ARGV[2]) || die "Can't open OUTPUT file - $!\n";
 
 my $record_count;
 
+print "Parsing Rows";
 while(<FILE>) {
-  $_ =~ s/\n|\r//g;
-  @record   = split(/,/);
-  $date = date2mysqldate($record[1]);
+  if($csv->parse($_)) {
+    my @columns = $csv->fields();
+    if($record_count % 100 == 0) { print "."; }
+    $date = date2mysqldate($columns[1]);
 
-  # Remove quotes & build Query
-  my $sql = "INSERT INTO ".$ARGV[0]." VALUES('0' ";
-  foreach $key (keys @record) {
-    $record[$key] =~ s/\"//g;
-    if($key == 1) {
-      $record[$key] = $date;
+    # Remove quotes & build Query
+    my $sql = qq/INSERT INTO $ARGV[0] VALUES('0' /;
+    foreach $key (keys @columns) {
+      $columns[$key] =~ s/\"//g;
+      if($key == 1) { # sub in date
+        $columns[$key] = $date;
+      }
+
+      # Escape sequence 3.2.1...
+      $columns[$key] =~ s/'/\'/g;
+      $columns[$key] =~ s/"/\"/g;
+      $columns[$key] =~ s/\\/\\\\/g;
+
+      $sql .= qq/, '$columns[$key]'/;
     }
-    $sql .= ", '".$record[$key]."'\n";
+    $sql .= ");\n";
+    print OUTFILE $sql."\n";
+    $record_count++;
   }
-  $sql .= ");\n";
-
-  print OUTFILE $sql;
-  $record_count++;
 }
-print "Processed $record_count records\n";
+print "\nProcessed $record_count records\n";
 
 sub date2mysqldate {
   $raw_date = shift;
+  print Dumper @_;
   $raw_date =~ s/\"//g;
   my($date, $time) = split(/ /, $raw_date);
 
